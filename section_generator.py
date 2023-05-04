ae495a3e80f7566ac562c869bb6aee651817c4cf
sc_ma <ccbora742@gmail.com>
2023-05-04 16:38:52 +0800

    Bug fix. Put all generators in auto_backgrounds.py.

--- section_generator.py
+++ section_generator.py
@@ -1,5 +1,6 @@
 from utils.prompts import generate_paper_prompts, generate_keywords_prompts, generate_experiments_prompts, generate_bg_summary_prompts
 from utils.gpt_interaction import get_responses, extract_responses, extract_keywords, extract_json
+from utils.figures import generate_random_figures
 import time
 import os
 
@@ -43,6 +44,38 @@ def section_generation_bg(paper, section, save_to_path, model):
     return usage
 
 
+def section_generation(paper, section, save_to_path, model):
+    """
+    The main pipeline of generating a section.
+        1. Generate prompts.
+        2. Get responses from AI assistant.
+        3. Extract the section text.
+        4. Save the text to .tex file.
+    :return usage
+    """
+    print(f"Generating {section}...")
+    prompts = generate_paper_prompts(paper, section)
+    gpt_response, usage = get_responses(prompts, model)
+    output = extract_responses(gpt_response)
+    paper["body"][section] = output
+    tex_file = os.path.join(save_to_path, f"{section}.tex")
+    # tex_file = save_to_path + f"/{section}.tex"
+    if section == "abstract":
+        with open(tex_file, "w") as f:
+            f.write(r"\begin{abstract}")
+        with open(tex_file, "a") as f:
+            f.write(output)
+        with open(tex_file, "a") as f:
+            f.write(r"\end{abstract}")
+    else:
+        with open(tex_file, "w") as f:
+            f.write(f"\section{{{section.upper()}}}\n")
+        with open(tex_file, "a") as f:
+            f.write(output)
+    # time.sleep(5)
+    print(f"{section} has been generated. Saved to {tex_file}.")
+    return usage
+
 def keywords_generation(input_dict,  model):
     title = input_dict.get("title")
     description = input_dict.get("description", "")
@@ -54,5 +87,9 @@ def keywords_generation(input_dict,  model):
     else:
         raise ValueError("`input_dict` must include the key 'title'.")
 
-def figures_generation():
-    pass
+def figures_generation(paper, save_to_path, model):
+    prompts = generate_experiments_prompts(paper)
+    gpt_response, usage = get_responses(prompts, model)
+    list_of_methods = list(extract_json(gpt_response))
+    generate_random_figures(list_of_methods, os.path.join(save_to_path, "comparison.png"))
+    return usage
