2c0ffedee9cf4f23977f3dea792bf297465587e6
sc_ma <ccbora742@gmail.com>
2023-05-04 18:16:44 +0800

    Retry 2 additional times to avoid random time out error.

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ from utils.file_operations import hash_name
 #   2. update QQ group and Organization cards
 #   4. add auto_polishing function
 #   5. Use some simple method for simple tasks (including: writing abstract, conclusion, generate keywords, generate figures...)
-#       5.1 Use GPT 3.5 for abstract, conclusion, ...
+#       5.1 Use GPT 3.5 for abstract, conclusion, ... (or may not)
 #       5.2 Use local LLM to generate keywords, figures, ...
 #       5.3 Use embedding to find most related papers (find a paper dataset)
-#       5.4 Use Segmantic Scholar API instead of Arxiv API.
+#       5.4 Use Semantic Scholar API instead of Arxiv API.
 #   6. get logs when the procedure is not completed.
 
 openai_key = os.getenv("OPENAI_API_KEY")
